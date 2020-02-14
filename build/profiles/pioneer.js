4f2f3bc231e076448322325e065ae70f8156579d
Anatoly Kovalchuk <akovalchuk@akovalchuk.ubuntu>
2020-02-14 12:02:29 +0300

    added gravity sensor

--- build/profiles/pioneer.js
+++ build/profiles/pioneer.js
@@ -4,10 +4,11 @@ Profiles.pioneer = {				// DO NOT FORGET TO CHANGE NAME OF PROFILE
 	WinLevel: 5,
 
 	Limits : {
-		'luminocity' : 1000,
+		'luminocity' : 2000,
 		'pressure' : 2.1, //pressure as Delta
-		'noise' : 33,
-		'accelerometer' : 20
+		'noise' : 33.4,
+		'accelerometer' : 20,
+                'gravity' : 9.8
 	},
 
 	Buildings : {
@@ -16,30 +17,35 @@ Profiles.pioneer = {				// DO NOT FORGET TO CHANGE NAME OF PROFILE
 			'pressure' : ["10_pressure"],
 			'noise' : ["10_noise"],
 			'accelerometer' : ["10_accelerometer"],
+                        'gravity' : ["10_gravity"],
 		},
 		'Samsung_82' : {
 			'luminocity' : ["82_luminocity"],
 			'pressure' : ["82_pressure"],
 			'noise' : ["82_noise"],
 			'accelerometer' : ["82_accelerometer"],
+                        'gravity' : ["82_gravity"],
 		},
 		'Samsung_87' : {
 			'luminocity' : ["87_luminocity"],
 			'pressure' : ["87_pressure"],
 			'noise' : ["87_noise"],
 			'accelerometer' : ["87_accelerometer"],
+                        'gravity' : ["87_gravity"],
 		},
 		'Samsung_88' : {
 			'luminocity' : ["88_luminocity"],
 			'pressure' : ["88_pressure"],
 			'noise' : ["88_noise"],
 			'accelerometer' : ["88_accelerometer"],
+                        'gravity' : ["88_gravity"],
 		},
 		'Samsung_92' : {
 			'luminocity' : ["92_luminocity"],
 			'pressure' : ["92_pressure"],
 			'noise' : ["92_noise"],
 			'accelerometer' : ["92_accelerometer"],
+                        'gravity' : ["92_gravity"],
 		},
 
 	}
